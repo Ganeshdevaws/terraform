e3650110a47a2db098ac5cd890a5885bdc041c97
ganesh <ganeshjanapareddy2019@gmail.com>
2023-10-12 17:10:08 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  default = "ami-03265a0778a880afb"
+}
+
+variable "instance_names" {
+  type = list
+  default = ["MongoDB", "Cart", "Catalogue", "User" , "Redis", "MySQL", "RabbitMQ", "Shipping", "Payment", "Web"]
+}
+
+variable "zone_id" {
+  default = "Z09525013PJ612FZXJ8ZJ"
+}
+
+variable "domain" {
+  default = "ganeshdevaws.online"
+}
---
+++ tfvars/PROD/prod.tfvars
@@ -0,0 +1,14 @@
+instances = {
+    MongoDB-PROD = "t3.large"
+    MySQL-PROD = "t3.medium"
+    Redis-PROD = "t3.micro"
+    RabbitMQ-PROD = "t3.micro"
+    Catalogue-PROD = "t3.micro"
+    User-PROD = "t3.micro"
+    Cart-PROD = "t3.micro"
+    Shipping-PROD = "t3.micro"
+    Payment-PROD = "t3.micro"
+    Web-PROD = "t3.micro"
+}
+
+sg_name = "allow-all-PROD"
